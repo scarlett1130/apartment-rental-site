8843c3efe91bd836fbb417992bde31ea1f73ccb9
scarlett1130 <businessx.free2000@gmail.com>
2022-07-08 17:34:53 +0000

    style: makes html and body occupy entire viewport

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,16 @@
+
 <template>
   <div>
+    <Head>
+      <Style>
+        html, body {
+          margin: 0;
+          padding: 0;
+          min-height: 100vh;
+          width: 100vw;
+        }
+      </Style>
+    </Head>
     <NuxtPage />
   </div>
 </template>
