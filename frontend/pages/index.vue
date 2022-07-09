ab77bcdb97284db9f409ca3d262c02374330357d
scarlett1130 <businessx.free2000@gmail.com>
2022-07-09 11:19:10 +0000

    style: fixes alignment in footer

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -469,7 +469,7 @@ const { data } = await useFetch<Apartment[]>('http://127.0.0.1:8000/v1/apartment
             </section>
         </main>
 
-        <footer class="mx-20 my-10 flex flex-row">
+        <footer class="mx-20 my-10 flex flex-row justify-between">
             <div class="flex flex-col w-1/2">
                 <span class="text-2xl">Logo</span>
                 <span>About us</span>
@@ -478,9 +478,9 @@ const { data } = await useFetch<Apartment[]>('http://127.0.0.1:8000/v1/apartment
             </div>
             <div class="w-1/2 flex flex-col">
                 <span>On a hunt? We can send you updates</span>
-                <div class="flex flex-row my-5">
+                <div class="flex flex-row my-5 justify-between">
                     <label for="email" class="hidden">Search</label>
-                    <input type="email" id="email" class="h-10 w-3/5 mr-5 bg-[#374151] pl-4 rounded-full text-white"
+                    <input type="email" id="email" class="h-10 w-4/5 mr-2 bg-[#374151] pl-4 rounded-full text-white"
                         placeholder="Email Address">
                     <Button label="Submit" class="p-button-outlined p-button-rounded" @click="" />
                 </div>
