fc9fd013a84b8187508b700ec6797ca49cdfd051
scarlett1130 <businessx.free2000@gmail.com>
2022-07-14 00:42:19 +0000

    fix: removes Carousel component from primevue

--- frontend/plugins/primevue.js
+++ frontend/plugins/primevue.js
@@ -1,11 +1,11 @@
 import { defineNuxtPlugin } from "#app";
 import PrimeVue from "primevue/config";
 import Button from "primevue/button";
-import Carousel from 'primevue/carousel';
+import Card from 'primevue/card'
 
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(PrimeVue, {ripple: true});
+    nuxtApp.vueApp.use(PrimeVue, { ripple: true });
     nuxtApp.vueApp.component('Button', Button);
-    nuxtApp.vueApp.component('Carousel', Carousel);
+    nuxtApp.vueApp.component('Card', Card)
     //other components that you need
 });
