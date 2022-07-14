00576a9d8a1fdd79c055699f3ab7cd0eb6ec2279
scarlett1130 <businessx.free2000@gmail.com>
2022-07-14 00:43:31 +0000

    feature: extracts footer and nav into components

---
+++ frontend/components/footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <footer class="mx-20 my-10 flex flex-row justify-between">
+        <div class="flex flex-col w-1/2">
+            <span class="text-2xl">Logo</span>
+            <span>About us</span>
+            <span>Contact us</span>
+
+        </div>
+        <div class="w-1/2 flex flex-col">
+            <span>On a hunt? We can send you updates</span>
+            <div class="flex flex-row my-5 justify-between">
+                <label for="email" class="hidden">Search</label>
+                <input type="email" id="email" class="h-10 w-4/5 mr-2 bg-[#374151] pl-4 rounded-full text-white"
+                    placeholder="Email Address">
+                <Button label="Submit" class="p-button-outlined p-button-rounded" @click="" />
+            </div>
+        </div>
+    </footer>
+</template>
---
+++ frontend/components/navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <nav ref="nav" class="flex flex-row justify-between items-center fixed inset-x-0 top-0 py-5 z-50 bg-gray-500">
+        <span class="m-0 text-2xl">Logo</span>
+        <label for="search" class="hidden">Search</label>
+        <input type="search" id="search" class="h-10 w-2/5 bg-[#374151] pl-4" placeholder="Enter A City/Region Here">
+    </nav>
+</template>
