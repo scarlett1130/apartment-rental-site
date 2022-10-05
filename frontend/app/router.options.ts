9656fa1f70569970d0bb2c2dc7cc13995d0342ce
scarlett1130 <businessx.free2000@gmail.com>
2022-10-05 17:28:27 +0000

    fix: removes console.log

--- frontend/app/router.options.ts
+++ frontend/app/router.options.ts
@@ -3,7 +3,6 @@ import type { RouterOptions } from '@nuxt/schema'
 // https://router.vuejs.org/api/interfaces/routeroptions.html
 export default <RouterOptions> {
     scrollBehavior: (to, from, savedPosition) => {
-        console.log(savedPosition)
         if (savedPosition) {
             return {top: savedPosition.top, behavior: 'smooth'}
         } else {
