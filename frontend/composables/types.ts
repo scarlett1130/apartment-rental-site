0ddde68572464fc8b237656211030619d61cde1a
scarlett1130 <businessx.free2000@gmail.com>
2022-10-05 18:31:34 +0000

    feature: adds apartmentFeature interface to ts definitions

--- frontend/composables/types.ts
+++ frontend/composables/types.ts
@@ -1,13 +1,19 @@
 export interface Apartment {
     id: number;
+    rooms?: number;
     name: string;
     location: ApartmentLocation;
     price: number;
     bedrooms: number;
     bathrooms: number;
     description: string;
-    image: ApartmentImage[];
+    apartment_image: ApartmentImage[];
     availability?: Date
+    apartment_type?: ApartmentType;
+    furnishing?: boolean;
+    kitchen?: boolean
+    garage?: boolean
+    apartment_features?: ApartmentFeature[];
 }
 
 export interface ApartmentLocation {
@@ -15,6 +21,7 @@ export interface ApartmentLocation {
     city: string;
     long: number;
     lat: number;
+    address?: string;
 }
 
 export interface ApartmentType {
@@ -26,4 +33,9 @@ export interface ApartmentType {
 export interface ApartmentImage {
     id: number;
     image: string;
+}
+
+export interface ApartmentFeature {
+    id: number;
+    name: string;
 }
