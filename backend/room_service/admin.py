317c09388718e6b74612674a9ff10b113350ea3a
scarlett1130 <businessx.free2000@gmail.com>
2022-10-05 18:55:24 +0000

    feature: registers Image and Feature in admin panel

--- backend/room_service/admin.py
+++ backend/room_service/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import ApartmentType, Location, Apartment
+from .models import ApartmentImage, ApartmentType, Location, Apartment, ApartmentFeature
 
 
 admin.site.register(ApartmentType)
 admin.site.register(Location)
 admin.site.register(Apartment)
+admin.site.register(ApartmentImage)
+admin.site.register(ApartmentFeature)
