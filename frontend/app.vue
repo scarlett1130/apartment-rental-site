64c60bff8770c6dc8c0dd7df4a34c7d87fb7fe7d
scarlett1130 <businessx.free2000@gmail.com>
2022-10-09 12:38:09 +0000

    inludes nuxtlayout

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,17 +1,17 @@
 
 <template>
-  <div>
-    <Head>
-      <Style>
-        body {
-          margin: 0;
-          padding: 0;
-          min-height: 100vh;
-        }
-      </Style>
-    </Head>
-    <NuxtLoadingIndicator />
-    <NuxtPage />
-    <Footer />
-  </div>
+  <NuxtLayout>
+    <div>
+      <NuxtLoadingIndicator />
+      <NuxtPage />
+    </div>
+  </NuxtLayout>
 </template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
+}
+</style>
