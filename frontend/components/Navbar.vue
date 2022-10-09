bd9e6d8bd4a8882645738f39932887f9de3c6e94
scarlett1130 <businessx.free2000@gmail.com>
2022-10-09 12:39:29 +0000

    feat: makes componrnt visibilty dynamic
    includes  'hidden' prop to toggle component visibility

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+
+interface Props {
+    hidden?: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+    hidden: false
+})
+
+</script>
+
 <template>
-    <nav ref="nav" class="flex flex-row justify-between items-center sticky inset-x-0 top-0 py-5 z-50 bg-white">
+    <nav class="flex flex-row justify-between items-center sticky inset-x-0 top-0 py-5 z-50 bg-white" :class="{'hidden': hidden}" >
         <span class="m-0 pl-5 text-2xl">
             <NuxtLink to="/">Logo</NuxtLink>
         </span>
