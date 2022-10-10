0168864cc00b45a4812eed8d86e412a2167b4780
scarlett1130 <businessx.free2000@gmail.com>
2022-10-10 17:08:45 +0000

    feat: adds search functionality to navbar

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -8,14 +8,32 @@ withDefaults(defineProps<Props>(), {
     hidden: false
 })
 
+const searchField = ref<string>("")
+
+const search = async (query: string) => {
+    console.log(query)
+    await navigateTo(
+        {
+            path: '/search',
+            query:
+            {
+                query: query
+            }
+        })
+}
+
 </script>
 
 <template>
-    <nav class="flex flex-row justify-between items-center sticky inset-x-0 top-0 py-5 z-50 bg-white" :class="{'hidden': hidden}" >
+    <nav class="flex flex-row justify-between items-center sticky inset-x-0 top-0 py-5 z-50 bg-white"
+        :class="{'hidden': hidden}">
         <span class="m-0 pl-5 text-2xl">
             <NuxtLink to="/">Logo</NuxtLink>
         </span>
         <label for="search" class="hidden">Search</label>
-        <input type="search" id="search" class="h-10 w-2/5 bg-[#374151] pl-4" placeholder="Enter A City/Region Here">
+        <form @submit.prevent="search(searchField)" class="h-10 w-2/5">
+            <input type="search" id="search" placeholder="Enter A City/Region Here"
+                class="bg-[#374151] w-full pl-4 h-full" v-model="searchField">
+        </form>
     </nav>
 </template>
