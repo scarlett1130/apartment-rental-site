dfbd8a1c3c8b6ca84af018233ed57c0f249bbebe
scarlett1130 <businessx.free2000@gmail.com>
2022-10-12 16:36:41 +0000

    chore: removes redundency

--- frontend/components/ApartmentCarouselItem.vue
+++ frontend/components/ApartmentCarouselItem.vue
@@ -7,10 +7,8 @@ defineProps<{ apartment: Apartment }>()
 <template>
     <div class="flex flex-col items-center mb-10">
         <NuxtLink :to="`/apartment/${apartment.id}`">
-            <div>
-                <img :src="apartment.apartment_image ? apartment.apartment_image[0].image : `https://picsum.photos/200/300`"
-                    alt="Apartment" class="apartment-image">
-            </div>
+            <img :src="apartment.apartment_image ? apartment.apartment_image[0].image : `https://picsum.photos/200/300`"
+                alt="Apartment" class="md:h-[15rem] h-[10rem] object-cover object-center">
             <div class="pt-2">
                 <span class="text-xl">{{ apartment.name }}</span>
                 <p class="">{{ apartment.location.city }}</p>
