71230e1aee7768ffc0d9c8139ce82cd9d085c3c3
scarlett1130 <businessx.free2000@gmail.com>
2022-10-12 10:34:53 +0000

    fix: fix search results not updating when searching on search results screen
    searching is now done onBeforeRouteUpdate when changing the route params
    through search

--- frontend/pages/search/index.vue
+++ frontend/pages/search/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { onBeforeRouteUpdate } from 'vue-router';
 import { Apartment } from '~~/composables/types';
 
 definePageMeta({
@@ -6,8 +7,24 @@ definePageMeta({
 })
 
 const route = useRoute()
+const apartments = ref<Apartment[]>([])
 
-const { data: apartments } = await useFetch<Apartment[]>(`http://localhost:8000/v1/apartments/`, { params: { search: route.query.searchQuery } })
+const search = async (query: string) => {
+    await useFetch<Apartment[]>(`http://localhost:8000/v1/apartments/`, { params: { search: query }, key: `apartments?${query}` })
+        .then((res) => {
+            apartments.value = res.data.value
+        })
+}
+
+onBeforeRouteUpdate((to, from, next) => {
+    if (to.query.searchQuery !== from.query.searchQuery) {
+        search(to.query.searchQuery as string)
+    }
+    next()
+})
+
+search(route.query.searchQuery as string)
+
 </script>
 
 <template>
@@ -16,8 +33,13 @@ const { data: apartments } = await useFetch<Apartment[]>(`http://localhost:8000/
             <h1 class="text-xl font-semibold">Showing results for {{route.query.searchQuery}}</h1>
         </template>
         <template #default>
-            <template v-for="apartment of apartments">
+            <template v-if="apartments.length" v-for="apartment of apartments">
                 <ApartmentItem :apartment="apartment" />
+            </template>
+            <template v-else>
+                <p>
+                    We are sorry, but we could not find any apartments matching your search query.
+                </p>
             </template>
         </template>
         <template #map>
