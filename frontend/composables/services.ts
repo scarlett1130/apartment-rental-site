a2d610673952c1f7ab1184091c61d1e06da6ecd5
scarlett1130 <businessx.free2000@gmail.com>
2022-10-15 17:58:34 +0000

    params is now optional with a default of {}

--- frontend/composables/services.ts
+++ frontend/composables/services.ts
@@ -4,6 +4,6 @@ const fetchOptions = {
     baseURL: "http://localhost:8000/v1/"
 }
 
-export const fetchApartments = (params: any) => {
+export const fetchApartments = (params: any = {}) => {
     return useFetch<Apartment[]>("apartments/", {...params, ...fetchOptions})
 }
