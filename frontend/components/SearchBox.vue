ed88a680ac17e5097494215668de5b5cdc64eae3
scarlett1130 <businessx.free2000@gmail.com>
2022-10-15 09:33:39 +0000

    feat: adds search btn that toggles on input

--- frontend/components/SearchBox.vue
+++ frontend/components/SearchBox.vue
@@ -23,9 +23,11 @@ const search = async () => {
 </script>
 
 <template>
-    <form @submit.prevent="search" :class="styleClass">
+    <form @submit.prevent="search" :class="styleClass" class="flex bg-[#374151] text-white">
         <label for="search" class="hidden">Search</label>
-        <input type="search" id="search" placeholder="Enter A City/Region Here" class="bg-[#374151] w-full pl-4 h-full"
+        <input type="search" id="search" placeholder="Enter A City/Region Here"
+            class="bg-[#374151] w-full focus-within:outline-none focus-visible:outline-none pl-4 h-full"
             v-model="searchField">
+        <input v-if="searchField.length" type="submit" value="Search" class="px-3">
     </form>
 </template>
