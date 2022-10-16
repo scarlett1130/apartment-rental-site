ab15e032d4b06e751cbccf7b17c2d8769d7c18a5
scarlett1130 <businessx.free2000@gmail.com>
2022-10-16 16:12:02 +0000

    feat: link is now only the image

--- frontend/components/ApartmentCarouselItem.vue
+++ frontend/components/ApartmentCarouselItem.vue
@@ -9,11 +9,11 @@ defineProps<{ apartment: Apartment }>()
         <NuxtLink :to="`/apartment/${apartment.id}`">
             <img :src="apartment.apartment_image ? apartment.apartment_image[0].image : `https://picsum.photos/200/300`"
                 alt="Apartment" class="md:h-[15rem] h-[10rem] object-cover object-center">
-            <div class="pt-2">
-                <span class="text-xl">{{ apartment.name }}</span>
-                <p class="">{{ apartment.location.city }}</p>
-                <p class="">{{ apartment.price }}</p>
-            </div>
         </NuxtLink>
+        <div class="pt-2">
+            <span class="text-xl">{{ apartment.name }}</span>
+            <p class="">{{ apartment.location.city }}</p>
+            <p class="">{{ apartment.price }}</p>
+        </div>
     </div>
 </template>
