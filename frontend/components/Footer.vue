be584e0e5691c351a4bd2bb3ddde4c29d9016e97
scarlett1130 <businessx.free2000@gmail.com>
2022-10-16 16:12:57 +0000

    style: makes responsive

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -1,12 +1,12 @@
 <template>
-    <footer class="mx-20 my-10 flex flex-row justify-between">
-        <div class="flex flex-col w-1/2">
+    <footer class="mx-5 md:mx-20 my-10 flex flex-col md:flex-row md:justify-between">
+        <div class="flex flex-col w-full md:w-1/2 order-2 md:order-none">
             <span class="text-2xl">Logo</span>
             <span>About us</span>
             <span>Contact us</span>
 
         </div>
-        <div class="w-1/2 flex flex-col">
+        <div class="w-full md:w-1/2 flex flex-col order-1 md:order-none">
             <span>On a hunt? We can send you updates</span>
             <div class="flex flex-row my-5 justify-between">
                 <label for="email" class="hidden">Search</label>
